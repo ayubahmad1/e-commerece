0c8eac950b2d2d30fb92d908a9be7c7b2687fdea
ayubahmad1 <ayub.ahmad@devsinc.com>
2022-08-04 14:22:36 +0500

    seed file updated and read me added

--- README.md
+++ README.md
@@ -1,24 +1,78 @@
-# README
+# E-Commerece
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+A simple E-commerece store to sell and purchase products.
 
-Things you may want to cover:
+## Ruby version
+- 2.7.0
 
-* Ruby version
+## Rails version
+- 5.2
 
-* System dependencies
+## Modules
+- Users
+- Products
+- Cart
+- Checkout
+- Coupon
 
-* Configuration
+## Features
+- A user can add or buy products
+- A user can comment to others products
+- A user can edit or delete his comments and products
+- A user can make his cart before login and can checkout after login to his account
+- A user can add his profile picture
+- A User can add many pictures for a product
+- Coupon can be used for discount if it is valid
 
-* Database creation
+Above are the some main features of my application.
 
-* Database initialization
+## System Dependencies
 
-* How to run the test suite
+I have used the following gems for my system:
 
-* Services (job queues, cache servers, search engines, etc.)
+- Pundit - For the authorization purpose in my app
+- Devise - Do all the authentication for my app
+- Cloudinary - Store my all the files on cloud.
+- Postgres - Used to manage my database
+- Rubocop - to style and manage my indentation of my code
+- Stripe - Manage checkouts and coupons
+- Bootstrap - Used to style my UI
+- Jquery - Used in place of javascript
+- byebug - To debug my application
 
-* Deployment instructions
+And of course i used visual studio as an editor.
 
-* ...
+## Configuration
+
+I have done some configurations for my application as:
+
+For sending emails:
+
+```sh
+config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.gmail.com',
+    port:           '587',
+    user_name:      'email@example.com',
+    password:       'password',
+    authentication: :plain
+    enable_starttls_auto: true
+  }
+```
+
+For cloudinary:
+
+```sh
+config.active_storage.service = :cloudinary
+```
+And for some other gems i install gems and make initializers for them.
+
+
+## Database
+```sh
+Postgres
+```
+#### Deployement
+
+I have deployed my application on heroku-18.
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,70 +8,74 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create!(username: 'user1', email: 'user1@gmail.com', password: '123456', password_confirmation: '123456',
-             confirmed_at: Time.zone.now)
-user2 = User.create!(username: 'user2', email: 'user2@gmail.com', password: '123456', password_confirmation: '123456',
-             confirmed_at: Time.zone.now)
+user1 = User.create!(name: 'user1', email: 'user11@gmail.com', password: '123456', password_confirmation: '123456')
+user2 = User.create!(name: 'user2', email: 'user22@gmail.com', password: '123456', password_confirmation: '123456')
 
 product1 = user1.products.create!(
-  title: 'Product 1',
-  description: 'This is a test produst 1',
-  price: 23
+  name: 'Product 1',
+  description: 'This is a test product 1',
+  quantity: 10,
+  price: 40_321
 )
 product1.images.attach(
-  io: File.open(Rails.root.join('app/assets/images/default1.jpg')),
-  filename: 'default1.jpg',
-  content_type: 'image/jpg'
+  io: File.open(Rails.root.join('app/assets/images/default1.jpeg')),
+  filename: 'default1.jpeg',
+  content_type: 'image/jpeg'
 )
 
 product2 = user1.products.create!(
-  title: 'Product 2',
+  name: 'Product 2',
   description: 'This is a test product 2',
-  price: 21
+  quantity: 10,
+  price: 21_321
 )
 product2.images.attach(
-  io: File.open(Rails.root.join('app/assets/images/default2.jpg')),
-  filename: 'default2.jpg',
-  content_type: 'image/jpg'
+  io: File.open(Rails.root.join('app/assets/images/default2.jpeg')),
+  filename: 'default2.jpeg',
+  content_type: 'image/jpeg'
 )
 
-product3 = user1.products.create!(title: 'Product 3',
+product3 = user1.products.create!(name: 'Product 3',
                                   description: 'This is a test produst 3',
-                                  price: 334)
+                                  quantity: 10,
+                                  price: 40_321)
 product3.images.attach(
-  io: File.open(Rails.root.join('app/assets/images/default3.jpg')),
-  filename: 'default3.jpg',
-  content_type: 'image/jpg'
+  io: File.open(Rails.root.join('app/assets/images/default3.jpeg')),
+  filename: 'default3.jpeg',
+  content_type: 'image/jpeg'
 )
 
 product4 = user2.products.create!(
-  title: 'Product 1',
-  description: 'This is a test produst 1',
-  price: 23
+  name: 'Product 4',
+  description: 'This is a test product 4',
+  quantity: 10,
+  price: 40_321
 )
 
 product4.images.attach(
-  io: File.open(Rails.root.join('app/assets/images/default1.jpg')),
-  filename: 'default1.jpg',
-  content_type: 'image/jpg'
+  io: File.open(Rails.root.join('app/assets/images/default4.jpeg')),
+  filename: 'default4.jpeg',
+  content_type: 'image/jpeg'
 )
 
 product5 = user2.products.create!(
-  title: 'Product 2',
-  description: 'This is a test product 2',
-  price: 21
+  name: 'Product 5',
+  description: 'This is a test product 5',
+  quantity: 10,
+  price: 40_321
 )
 product5.images.attach(
-  io: File.open(Rails.root.join('app/assets/images/default2.jpg')),
-  filename: 'default2.jpg',
-  content_type: 'image/jpg'
+  io: File.open(Rails.root.join('app/assets/images/default5.jpeg')),
+  filename: 'default5.jpeg',
+  content_type: 'image/jpeg'
 )
 
-product6 = user2.products.create!(title: 'Product 3',
+product6 = user2.products.create!(name: 'Product 6',
                                   description: 'This is a test produst 3',
-                                  price: 334)
+                                  quantity: 10,
+                                  price: 40_321)
 product6.images.attach(
-  io: File.open(Rails.root.join('app/assets/images/default3.jpg')),
-  filename: 'default3.jpg',
-  content_type: 'image/jpg'
+  io: File.open(Rails.root.join('app/assets/images/default6.jpeg')),
+  filename: 'default6.jpeg',
+  content_type: 'image/jpeg'
 )
