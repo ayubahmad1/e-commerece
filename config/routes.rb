04a74b005b66fd9f756d2aebb4316219344317ea
ayubahmad1 <ayub.ahmad@devsinc.com>
2022-08-04 16:37:06 +0500

    AJAX of comments fix

--- config/routes.rb
+++ config/routes.rb
@@ -12,22 +12,9 @@ Rails.application.routes.draw do
 
   resource :cart, only: :show
 
-
-
-  # resources :checkout, only: [:create] do
-  #   get 'success', as: "success"
-  #   get 'cancel', as: "cancel"
-  # end
   post "checkout/create", to: "checkout#create", as: "checkout"
   get "success", to: "checkout#success", as: "success"
   get "cancel", to: "checkout#cancel", as: "cancel"
-
-  # get "products/all_users_products", to: "products#all_users_products", on: :collection
-
-
-  # put 'products/update_quantity/:id', to: 'products#update_quantity', as: 'update_quantity'
-  # post 'products/add_to_cart/:id', to: 'products#add_to_cart', as: 'add_to_cart'
-  # delete 'products/remove_from_cart/:id', to: 'products#remove_from_cart', as: 'remove_from_cart'
 
   put 'carts/update_quantity/:id', to: 'carts#update_quantity', as: 'update_quantity'
   post 'carts/add_to_cart/:id', to: 'carts#add_to_cart', as: 'add_to_cart'
