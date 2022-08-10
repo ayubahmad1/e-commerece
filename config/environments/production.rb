f8e59c55b965db775f3a1d52b7fe3b5ccb8f871d
ayubahmad1 <ayub.ahmad@devsinc.com>
2022-08-10 14:50:19 +0500

    change configuration in Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.serve_static_files = true
   # config.assets.compress = true
 
 
